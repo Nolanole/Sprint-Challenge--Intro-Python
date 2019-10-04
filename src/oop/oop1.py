382f953695acff8039f8596383acfa2970c89273
Josh Mancuso <30558826+Nolanole@users.noreply.github.com>
2019-10-04 11:54:05 -0500

    uploaded sprint challenge submission

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,4 +1,4 @@
-# Write classes for the following class hierarchy:
+ # Write classes for the following class hierarchy:
 #
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+  pass
+
+class GroundVehicle(Vehicle):
+  pass
+
+class Car(GroundVehicle):
+  pass
+
+class Motorcycle(GroundVehicle):
+  pass
+
+class FlightVehicle(Vehicle):
+  pass
+
+class Starship(FlightVehicle):
+  pass
+
+class Airplane(FlightVehicle):
+  pass
